49f6c8aab5b4081602a0b00a861b83195378b049
Boon Kiat Jeffrey Ng <s10197583@connect.np.edu.sg>
2020-11-08 20:25:42 +0800

    Linkage, Sending, Retrieving Specific Data from FireBase on a small scale function added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,5 +37,76 @@ namespace SpaceAndGo.Controllers
         }
 
         //Testing
+
+        //Edit to DB 
+        //https://developer.okta.com/blog/2019/04/30/store-data-firebase-aspnet-mvc-csharp
+        public async Task<ActionResult> About()
+        {
+            //Simulate test user data and login timestamp
+            var userId = "123456";
+            var currentLoginTime = DateTime.UtcNow.ToString("MM/dd/yyyy HH:mm:ss");
+
+            //Save non identifying data to Firebase
+            var currentUserLogin = new LoginData() { TimestampUtc = currentLoginTime };
+            var firebaseClient = new FirebaseClient("https://spaceandscan.firebaseio.com/");
+            var result = await firebaseClient
+              .Child("Users/" + userId + "/Logins")
+              .PostAsync(currentUserLogin);
+
+            //Retrieve data from Firebase
+            var dbLogins = await firebaseClient
+              .Child("Users")
+              .Child(userId)
+              .Child("Logins")
+              .OnceAsync<LoginData>();
+
+            var timestampList = new List<DateTime>();
+
+            //Convert JSON data to original datatype
+            foreach (var login in dbLogins)
+            {
+                timestampList.Add(Convert.ToDateTime(login.Object.TimestampUtc).ToLocalTime());
+            }
+
+            //Pass data to the view
+            ViewBag.CurrentUser = userId;
+            ViewBag.Logins = timestampList.OrderByDescending(x => x);
+            return View();
+        }
+
+        public async Task<ActionResult> ViewMap()
+        {
+            var location = "Block 1";
+            //var crowd = "5";
+            //var userId = "123456";
+            //var currentLoginTime = DateTime.UtcNow.ToString("MM/dd/yyyy HH:mm:ss");
+
+            //Save non identifying data to Firebase
+        //    var currentUserLogin = new LocationData() { CrowdNow = crowd  };
+            var firebaseClient = new FirebaseClient("https://spaceandscan.firebaseio.com/");
+            //var result = await firebaseClient
+              //.Child("Location/" + location)
+              //.PostAsync(currentUserLogin);
+
+            //Retrieve data from Firebase
+            var dbLogins = await firebaseClient
+              .Child("Location")
+              .Child(location)
+              .OnceAsync<LocationData>();
+            var tempList = new List<string>();
+            foreach (var login in dbLogins)
+            {
+                //ViewBag.Crowd = login.Object.CrowdNow;
+                tempList.Add((login.Object.CrowdNow).ToString());
+            }
+            //MAYBE SOMEONE CAN FIND ANOTHER WAY TO GET DATA FROM FIREBASE.OBJECT
+            //Pass data to the view
+            ViewBag.Location = location;
+            //ViewBag.temp = dbLogins.ToString();    HOW TO CONVERT DIRECTLY??
+            ViewBag.Crowd = tempList;
+            return View();
+        }
+
+
     }
 }
---
+++ Models/LocationData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceAndGo.Models
+{
+    public class LocationData
+    {
+
+        public string CrowdNow { get; set; }
+    }
+}
