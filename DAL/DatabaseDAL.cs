fdcefd20fc1b4c71bb7fa3c67dde151a469924c2
Aaron Choo <s10194746@connect.np.edu.sg>
2020-11-12 12:52:24 +0800

    DAL incomplete

---
+++ DAL/DatabaseDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SpaceAndGo.Models;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace SpaceAndGo.DAL
+{
+    public class DatabaseDAL
+    {
+
+    }
+}
