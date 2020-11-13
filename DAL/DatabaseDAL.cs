2461d6e46b9e698f31e2a49ff47f2002492c8ff0
Aaron Choo <s10194746@connect.np.edu.sg>
2020-11-13 10:15:54 +0800

    Installed SqlClient package for DAL

--- DAL/DatabaseDAL.cs
+++ DAL/DatabaseDAL.cs
@@ -8,11 +8,29 @@ using Microsoft.Extensions.Logging;
 using SpaceAndGo.Models;
 using Firebase.Database;
 using Firebase.Database.Query;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.SqlClient;
 
 namespace SpaceAndGo.DAL
 {
     public class DatabaseDAL
     {
-
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+        //Constructor     
+        public DatabaseDAL()
+        {
+            //Read ConnectionString from appsettings.json file       
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+                "Air_Flights_Management_System_ConnectionString");
+            //Instantiate a SqlConnection object with the         
+            //Connection String read.          
+            conn = new SqlConnection(strConn);   //conn is like the key to database
+        }
     }
 }
