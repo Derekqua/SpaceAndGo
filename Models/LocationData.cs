3f113458862d1ae665723d790c896995bc19fb8b
Aaron-CHM <64004039+Aaron-CHM@users.noreply.github.com>
2020-11-14 22:02:17 +0800

    DAL connection done but only with Update CrowdNow method; changed LocationData model to be both int like in database; modified corresponding lists in HomeController

--- Models/LocationData.cs
+++ Models/LocationData.cs
@@ -5,14 +5,14 @@ using System.Threading.Tasks;
 
 namespace SpaceAndGo.Models
 {
-    public class DataContext : IDisposable
-    public partial class LocationData : DataContext
+    //public class DataContext : IDisposable
+    public /*partial*/ class LocationData //: DataContext
     {
-        public string Location { get; set; }
-        public string CrowdNow { get; set; }
+        public int Location { get; set; }
+        public int CrowdNow { get; set; }
 
-        public Table<Locations> Locations;
+        /*public Table<Locations> Locations;
         public Table<LCount> LCount;
-        public LocationData(string connection) : base(connection) { }
+        public LocationData(string connection) : base(connection) { }*/
     }
 }
