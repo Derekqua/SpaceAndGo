5aa07e234a4c015a4d8fcf1afe26e176af5fb627
floofpotats <jolene.cheong@yahoo.com.sg>
2020-11-18 16:03:36 +0800

    coding colours

---
+++ wwwroot/js/map.js
@@ -0,0 +1,19 @@
+﻿
+@foreach (var Object in ViewBag.Object)
+{
+    if (@Object.Location=="Block 1 ")
+    {
+        if (@Object.CrowdNow <= 50)
+        {
+    document.getElementById("blk1").style.fill = "green";
+        }
+        else if (@Object.CrowdNow <= 100)
+        {
+    document.getElementById("blk1").style.fill = "orange";
+        }
+        else
+        {
+    document.getElementById("blk1").style.fill = "red";
+        }
+    }
+}
