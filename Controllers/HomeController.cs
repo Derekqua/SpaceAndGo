29ddaf83a17b9f45a138941acbf7d518a50013f8
Jun Yuan Derek Qua <s10196775@connect.np.edu.sg>
2021-01-28 10:22:39 +0800

    Sending News to Email Completed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,11 @@ using Firebase.Database.Query;
 using System.Net.Http;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using System.Net;
+using System.Net.Mail;
+
+using Quartz;
+using Quartz.Impl;
 
 namespace SpaceAndGo.Controllers
 {
@@ -183,6 +188,88 @@ namespace SpaceAndGo.Controllers
             }
             
         }
+        [HttpGet]
+        public ActionResult Email()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Email(Form email)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Article[] articles = { };
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://covid-19-news.p.rapidapi.com/v1/covid?q=covid&lang=en&media=True"),
+                    Headers =
+                {
+                    { "x-rapidapi-key", "c947943dd5mshd3da70c50ce7bb9p175bd6jsndb5705073769" },
+                    { "x-rapidapi-host", "covid-19-news.p.rapidapi.com" },
+                },
+                };
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    Rootobject rootdata = JsonConvert.DeserializeObject<Rootobject>(body);
+                    articles = rootdata.articles;
+                }
+
+                //setting content in email
+                var content = "";
+                var num = 1;
+                foreach (var i in articles)
+                {
+                    content += "<h1>" + num + ". " + i.title + "</h1><br>";
+                    content += "<a href=" + i.link + "><img src=" + i.media + " /></a>";
+                    content += i.summary;
+                    num += 1;
+                }
+
+                //sending email
+                var message = new MailMessage();
+                message.To.Add(new MailAddress(email.FromEmail));  // replace with valid value 
+                message.From = new MailAddress("derekqua8@gmail.com");  // replace with valid value
+                message.Subject = "Space & Go";
+                message.Body = content;
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "spacengo198@gmail.com",  // replace with valid value
+                        Password = "Space198!Go"  // replace with valid value
+                    };
+                 
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    TempData["errorMessage"] = "Email Sent Successfuly. You will receive news daily.";
+                    return View();
+                }
+            }
+            else
+            {
+                // Store an error message in TempData for display at the CreateRoute view     
+                TempData["errorMessage"] = "Failed to send an Email, Please Try Again!";
+
+                //Input validation fails, return to the Create view
+                //to display error message
+                return View(email);
+            }
+        }
+
+
 
     }
 }
---
+++ Models/Form.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaceAndGo.Models
+{
+    public class Form
+    {
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string FromEmail { get; set; }
+    }
+}
